16a4aa670e05b69f74d59699ed0cf61d4d9b3c96
Dennis Trautwein <git@dtrautwein.eu>
2023-10-13 19:04:42 +0200

    WIP

--- fullrt_test.go
+++ fullrt_test.go
@@ -1,23 +1,15 @@
 package zikade
 
-import (
-	"context"
-	"testing"
-	"time"
-
-	"github.com/stretchr/testify/require"
-)
-
-func TestNewFullRT(t *testing.T) {
-	cfg := &FullRTConfig{
-		Config:        DefaultConfig(),
-		CrawlInterval: 60 * time.Minute,
-	}
-	h := newTestHost(t)
-	fullRT, err := NewFullRT(h, cfg)
-	require.NoError(t, err)
-
-	fullRT.Bootstrap(context.Background())
-
-	time.Sleep(time.Hour)
-}
+//func TestNewFullRT(t *testing.T) {
+//	cfg := &FullRTConfig{
+//		Config:        DefaultConfig(),
+//		CrawlInterval: 60 * time.Minute,
+//	}
+//	h := newTestHost(t)
+//	fullRT, err := NewFullRT(h, cfg)
+//	require.NoError(t, err)
+//
+//	fullRT.Bootstrap(context.Background())
+//
+//	time.Sleep(time.Hour)
+//}
